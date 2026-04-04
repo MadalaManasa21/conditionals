1136e4deae9bc603fffb37bb3902c5a8f31bf6be
MadalaManasa21 <manasamadala35@gmail.com>
2026-04-04 11:48:09 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,11 @@
+print('hi')
+a=int(input('enter a number'))
+if a==4:
+    print('hi')
+    print('inside')
+elif a<6:
+        print('a is 6')
+        print('i am inside elif')
+else:
+        print('a is not 4')
+        print('outside')
