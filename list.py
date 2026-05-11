de1232f316d890cfd4ff7f5c6a626058b25e26f4
MadalaManasa21 <manasamadala35@gmail.com>
2026-05-11 22:43:00 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,23 @@
+'''
+list
+'''
+#methods
+#append
+#extend
+#remove
+#count
+#pop
+#index
+a=[1,24,1,4,1544,5,"python life"]
+#print(type(a)
+#print(a[0:4:2])
+print(a[1])
+a=[1,24,1,4,1544,5,"python life"]
+#a.append("kiran")
+#a.extend([2,3,45,56])
+#print(a.count(1))
+#print(a)
+#a.remove(24)
+#a.pop(1)
+print(a.index(24))
+print(a)
