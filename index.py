e3c89044bc4f58eda0727346b1056326ccd5283c
MadalaManasa21 <manasamadala35@gmail.com>
2026-07-21 19:52:45 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,29 @@
+s="python indexing helps us acess character from both positive and negative position in a string"
+#positive indexing
+print(s[0])
+print(s[7])
+print(s[15])
+print(s[25])
+print(s[40])
+print(s[50])
+print(s[60])
+print(s[75])
+print(s[85])
+print(s[len(s) -1])
+#negative indexing
+print(s[-1])
+print(s[-5])
+print(s[-10])
+print(s[-15])
+print(s[-20])
+print(s[-25])
+print(s[-30])
+print(s[-40])
+print(s[-50])
+print(s[-len(s)])
+
+
+
+
+
+
